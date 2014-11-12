b9a052aedd8769d0f2b4704086e39765bd4a7af1
nuchticha <nuchticharadchompu@outlook.com>
2014-11-12 16:30:19 +0700

    1

--- main.c
+++ main.c
@@ -59,23 +59,3 @@ int main()
 }
 
 
-
-
-/*
-
-void ethread_create(pthread_t *thread, const pthread_attr_t *attr, 
-                    void (*start_routine)(void *), void *arg)
-{
-  if(pthread_create(thread,attr,(void *)start_routine,arg)){
-    perror("pthread_create");
-    exit(0);
-  }
-}
-
-void ethread_exit(void *value_ptr)
-{
-  pthread_exit(value_ptr);
-}
-
-
-*/
